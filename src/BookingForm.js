d9e41d3ad4f256bcca2ec25ed47be8c5cda53987
Timo ELony <timo.elony@gmail.com>
2025-03-29 13:57:00 +0300

    bugfixes and mapping available times instead of rewriting them

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import {useState } from "react";
 
 
 export default function BookingForm () {
+    const [availableTimes, setAvailableTimes] = useState(
+        [
+            '17:00',
+            '18:00',
+            '19:00',
+            '20:00',
+            '21:00',
+            '22:00'
+        ]
+    );
     const [formData, setFormData] = useState(
         {
             date: '',
@@ -19,14 +29,14 @@ export default function BookingForm () {
                 occasion: ''
             }
         )
-    }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         alert('submitted');
         e.target.reset();
         resetForm();
-    }
+    };
 
     const handleChange = (e) => {
         setFormData(
@@ -35,7 +45,7 @@ export default function BookingForm () {
                 [e.target.id]: e.target.value
             }
         );
-    }
+    };
     return(
         <>
             <h1>Book your table</h1>
@@ -43,23 +53,22 @@ export default function BookingForm () {
                 <label htmlFor="date">Choose date</label>
                 <input type="date" id="date" value={formData.date} onChange={handleChange}/>
                 <label htmlFor="time">Choose time</label>
-                <select id="time " value={formData.time} onChange={handleChange} required>
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
+                <select id="time" value={formData.time} onChange={handleChange} required>
+                    {
+                        availableTimes.map((time, index)=> (
+                            <option key={index} value={time}>{time}</option>
+                        ))
+                    }
                 </select>
                     <label htmlFor="guests">Number of guests</label>
-                    <input type="number" placeholder="1" min="1" max="10" id="guests" value={formData.value} onChange={handleChange}/>
+                    <input type="number" placeholder="1" min="1" max="10" id="guests" value={formData.guests} onChange={handleChange}/>
                         <label htmlFor="occasion">Occasion</label>
                         <select id="occasion" value={formData.occasion} onChange={handleChange}>
-                            <option>Birthday</option>
-                            <option>Anniversary</option>
+                            <option value={'Birthday'}>Birthday</option>
+                            <option value={'Anniversary'}>Anniversary</option>
                         </select>
                         <input type="submit" value="Make Your reservation"/>
             </form>
         </>
     );
-}
+};
